cf6a38b15b755a019f89721f87aa5fe2f8aa66ce
Tosanwumi Abugewa-Ejegi <tosanwumiae@gmail.com>
2025-01-13 11:58:34 +0100

    Add new comment feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,51 @@ export default function App() {
   const { currentUser, comments: commentsData } = data;
   const [comments, setComments] = useState(commentsData)
 
+  //Helper function
+  function generateUniqueId() {
+    const timestamp = Date.now(); // Current timestamp in milliseconds
+    const randomNum = Math.floor(Math.random() * 1000000); // Random number between 0 and 999999
+    return `${timestamp}-${randomNum}`;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formElement = event.target;
+    const formData = new FormData(formElement);
+
+    addNewComment(formData);
+    formElement.reset();
+  }
+
+  function addNewComment(formData, formElement) {
+    const comment = formData.get('comment');
+    if (comment === '') {
+      return;
+    };
+    setComments(prevComments => (
+      [
+        ...prevComments,
+        {
+          "id": generateUniqueId(),
+          "content": comment,
+          "createdAt": "1 month ago",
+          "score": 0,
+          "user": {...currentUser},
+          "replies": []
+        },
+      ]
+    ))
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      const formElement = event.currentTarget;
+      const formData = new FormData(formElement);
+      addNewComment(formData);
+      formElement.reset();
+    } else {return}    
+  }
+
   return (
     <main>
       {comments.map(comment => {
@@ -28,7 +73,7 @@ export default function App() {
           </div>
         )
       })}
-      <form action="#" method='post' className="create-comment">
+      <form action="#" onSubmit={handleSubmit} onKeyDown={handleKeyDown} method='post' className="create-comment">
         <img src={currentUser.image.png} alt="" />
         <textarea name="comment" id="comment" rows={5} placeholder='Add a comment...'></textarea>
         <button>Send</button>
